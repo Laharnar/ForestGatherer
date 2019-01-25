389b3f0e3d51d66ec181318abf141bf5aed28e14
Laharnar <marko.laharnar@gmail.com>
2019-01-25 22:33:53 +0100

    end menus, pickup

---
+++ ForestGatherers/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager m;
+
+    public Transform uiObj;
+
+    private void Start() {
+        m = this;
+    }
+
+    internal void ShowUI(string uiName) {
+        for (int i = 0; i < uiObj.childCount; i++) {
+            uiObj.GetChild(i).gameObject.SetActive(false);
+        }
+        uiObj.Find(uiName).gameObject.SetActive(true);
+
+    }
+}
---
+++ ForestGatherers/Assets/Scripts/PickupSystem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSystem : MonoBehaviour
+{
+    public List<PickupItem> collectedItems = new List<PickupItem>();
+    public List<PickupItem> points = new List<PickupItem>();
+
+    public Transform housePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (points.Count > 0 && points[0].transform == housePoint) {
+            
+            EndGame();
+        }
+    }
+
+    private void EndGame() {
+        Debug.Log("End game");
+        GameManager.m.ShowUI("Menu1_END");
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        PickupItem item = collider.GetComponent<PickupItem>();
+        if (item != null) {
+            item.gameObject.SetActive(false);
+            if (item.mode == 0)
+                collectedItems.Add(item);
+            else 
+                points.Add(item);
+        }
+    }
+}
