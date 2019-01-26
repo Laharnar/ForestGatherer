cf85e40aa10b19de8ed4d44ef06a3a77c92d98ee
Laharnar <marko.laharnar@gmail.com>
2019-01-26 18:13:54 +0100

    terrain(ignored)

---
+++ ForestGatherers/Assets/Scripts/TreeChoices.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeChoices : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    [ContextMenu("Pick tree")]
+    private void PickOneTree() {
+        PickOneTree(UnityEngine.Random.Range(0, transform.childCount));
+    }
+
+    private void PickOneTree(int v) {
+        for (int i = 0; i < transform.childCount; i++) {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+        transform.GetChild(v).gameObject.SetActive(true);
+    }
+}
